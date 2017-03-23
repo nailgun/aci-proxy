665526fb75c39241577ea8f30969e41d423ca37b
Dmitry Bashkatov <dbashkatov@gmail.com>
2017-03-23 23:56:34 +0300

    update CA creds

--- gen_ca.sh
+++ gen_ca.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-openssl req -new -newkey rsa:2048 -subj '/CN=Squid CA' -sha256 -days 36500 -nodes -x509 -extensions v3_ca -keyout ca-key.pem -out ca.pem
+openssl req -new -newkey rsa:2048 -subj '/CN=ACI-proxy CA' -sha256 -days 36500 -nodes -x509 -extensions v3_ca -keyout ca-key.pem -out ca.pem
 cat ca-key.pem ca.pem > ca-bundle.pem
 rm ca-key.pem
 openssl x509 -in ca.pem -text
